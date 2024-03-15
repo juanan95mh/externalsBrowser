87d2aa6c6563837ae280d17aa78b8dd65de2358e
Juan Andres Martínez Hontanaya <45402796+juanan95mh@users.noreply.github.com>
2024-03-15 07:31:35 +0100

    Update externalsBrowser.py
    url error

--- externalsBrowser.py
+++ externalsBrowser.py
@@ -20,7 +20,11 @@ def main():
 		print(f"{i}. {external}")
 	choice = int(input("Pick an external: ")) - 1
 	if 0 <= choice < len(externals):
-		open_in_rabbitvcs(externals[choice])
+		url = externals[choice]
+		#Split url to get the path
+		url = url.split()[0]
+		print (f"Opening {url} in RabbitVCS")
+		open_in_rabbitvcs(url)
 	else:
 		print("Invalid choice")
 
